b4ba9c88e0771116d9d9ce80ee07764cdedef460
Mark Todisco <marktodisco@gmail.com>
2020-10-15 21:39:34 -0400

    Re-added install_requires and python_requires information.

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='solids',
+    packages=find_packages(),
     version='0.0.1',
     install_requires=['sympy', 'numpy', 'matplotlib', 'scipy'],
     python_requires='>=3.7, <3.8'
