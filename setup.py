0741b03c66e48410ba227ee60eb6b476b22ffc6b
Mark Todisco <marktodisco@gmail.com>
2020-11-10 00:47:38 -0500

    Removed scipy from setup file.

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 from setuptools import setup, find_packages
 
-
 setup(
     name='solids',
     packages=find_packages(),
     version='0.0.1',
-    install_requires=['sympy', 'numpy', 'matplotlib', 'scipy'],
+    install_requires=['sympy', 'numpy', 'matplotlib'],
     python_requires='>=3.7, <3.8'
 )
